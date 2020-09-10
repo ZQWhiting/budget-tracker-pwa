0024df83f949094d7f3708cfc316f4e0db1845a0
ZQWhiting <zach.whiting@icloud.com>
2020-09-10 14:10:45 -0600

    transactions are now saved in indexeddb and are uploaded when connection returns

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,16 +1,16 @@
 let db;
-const request = indexedDB.open('budget_tracker', 1);
+const request = indexedDB.open('transaction_tracker', 1);
 
 request.onupgradeneeded = function(e) {
     const db = e.target.result;
-    db.createObjectStore('new_budget', {autoIncrement: true});
+    db.createObjectStore('new_transaction', {autoIncrement: true});
 }
 
 request.onsuccess = function(e) {
     db = e.target.result;
 
     if (navigator.online) {
-        // uploadBudget();
+        uploadTransaction();
     }
 }
 
@@ -19,9 +19,39 @@ request.onerror = function(e) {
 }
 
 function saveRecord(record) {
-    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    const budgetObjectStore = transaction.objectStore('new_budget');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
 
-    budgetObjectStore.add(record)
+    transactionObjectStore.add(record)
 }
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    const getAll = transactionObjectStore.getAll();
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+                const transactionObjectStore = transaction.objectStore('new_transaction');
+                transactionObjectStore.clear();
+            })
+            .catch(err => console.log(err))
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransaction)
